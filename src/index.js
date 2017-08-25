1a49ecf120b2c4cacb25a3e95a7ebffad85e8698
Mikolaj Kojdecki <mikolaj.kojdecki@polidea.com>
2017-08-25 18:14:32 +0200

    Added Apollo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloClient } from 'react-apollo'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    networkInterface: createNetworkInterface({
+      uri: 'http://3b62e363.ngrok.io',
+    }),
+  });
+
+ReactDOM.render(
+<App> 
+    <ApolloProvider client={client}>
+        <MyRootComponent/>
+    </ApolloProvider>,
+</App>, 
+document.getElementById('root'));
+
 registerServiceWorker();
