193c8d6d264d4057d53315679dc54d3fc6f8c207
Mikolaj Kojdecki <mikolaj.kojdecki@polidea.com>
2017-08-25 18:18:25 +0200

    Fixed Apollo

--- src/index.js
+++ src/index.js
@@ -3,20 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { ApolloClient } from 'react-apollo'
+import { ApolloClient, createNetworkInterface, ApolloProvider } from 'react-apollo'
 
 const client = new ApolloClient({
     networkInterface: createNetworkInterface({
-      uri: 'http://3b62e363.ngrok.io',
+      uri: 'http://b3f241e3.ngrok.io/',
     }),
   });
 
 ReactDOM.render(
-<App> 
     <ApolloProvider client={client}>
-        <MyRootComponent/>
-    </ApolloProvider>,
-</App>, 
+        <App/>
+    </ApolloProvider>, 
 document.getElementById('root'));
 
 registerServiceWorker();
